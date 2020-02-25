5737c78542f0b60edbd954f56f96edbf30bfd9df
BasemMohamed <basem.abdulrahem@gmail.com>
2020-02-25 17:12:34 +0200

    Update GetCVE.sh

--- GetCVE.sh
+++ GetCVE.sh
@@ -8,5 +8,5 @@ nmap -vv -A -T4 -oA scanresult $ip | grep -Eo '^[0-9]{1,6}' > openports.csv
 
 while read openPort;
 do 
-      echo `nmap -Pn -p $openPort --script vuln $ip | grep 'CVE:' | cut -d':' -f3` &
+      nmap -Pn -p $openPort --script vuln $ip | grep 'CVE:' | cut -d':' -f3 >> CVEs.output
 done < openports.csv
